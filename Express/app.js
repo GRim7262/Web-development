14bb6335a44f69c73e14dd9c4ec34c4a31972d4d
GRim7262 <vinalpawar1@gmail.com>
2023-02-21 16:15:08 +0530

    files added

---
+++ Express/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 80;
+const fs = require("fs");
+
+//EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) //For serving static files
+app.use(express.urlencoded({extended: true}))
+
+//PUG SPECIFIC STUFF
+app.set('view engine', 'pug') //set the template engine as pug
+app.set('views', path.join(__dirname, 'views')); //set the view directory
+
+//ENDPOINTS
+app.get('/', (req, res) => {
+    const con = "This is a content";
+    const params = {'title': 'Pub template', "content": con}
+    res.status(200).render('index.pug', params);
+});
+
+app.post('/', (req, res) => {
+    name = req.body.name
+    age = req.body.age
+    gender = req.body.gender
+    address = req.body.address
+    more = req.body.more
+    let outputToWrite = `The name of the client is ${name}, ${age} years old, ${gender}, residing at ${address}. More about him.her: ${more}`
+    fs.writeFileSync('output.txt',outputToWrite)
+    const params = {'message': 'Your form has been submitted successfully'}
+    res.status(200).render('index.pug', params);
+});
+
+//START THE SERVER
+app.listen(port, () => {
+    console.log(`The application started successfully on port ${port}`);
+});
